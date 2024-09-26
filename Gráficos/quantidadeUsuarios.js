20826415d5ba9b3b9deafc41e86c6046a53c67aa
Anny <104777994+Any09@users.noreply.github.com>
2024-09-26 15:00:53 -0300

    vbnfgn

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -31,7 +31,7 @@ async function quantidadeUsuarios() {
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Anos',
+                text: 'Período',
                 font: {
                     color: getCSS('--cor-secundaria')
                 }
@@ -40,7 +40,7 @@ async function quantidadeUsuarios() {
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Bilhões de usuários ativos',
+                text: '',
                 font: {
                     color: getCSS('--cor-secundaria')
                 }
