3048ab54f4e92f657872de12a66c27b194c37fc7
Anny <104777994+Any09@users.noreply.github.com>
2024-09-26 15:05:22 -0300

    rtyrty

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -21,7 +21,7 @@ async function quantidadeUsuarios() {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
         title: {
-            text: 'Redes sociais com mais usuários no mundo',
+            text: 'Linha temporal de africanos vindos para o Brasil',
             font: {
                 color: getCSS('--cor-principal'),
                 family: getCSS('--fonte'),
